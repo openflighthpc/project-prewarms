cfb8dbb4f2e86ed763302f001f5dfa276cdc5972
Stu Franks <stu@stuts.uk>
2022-05-11 16:24:35 +0100

    Fix bowtie job - FastA > FastQ

--- life-sciences-bowtie.sh
+++ life-sciences-bowtie.sh
@@ -8,11 +8,11 @@ spack install bowtie
 mkdir life-sciences-ecoli-demo
 cd life-sciences-ecoli-demo
 
-wget -O ecoli.fq https://raw.githubusercontent.com/BenLangmead/bowtie/master/reads/e_coli_1000.fq
+wget -O ecoli.fa https://raw.githubusercontent.com/BenLangmead/bowtie/master/reads/e_coli_1000.fa
 cat << EOF > ecoli.sh
 #!/bin/bash -l
 #SBATCH -N 1
 flight env activate spack
 spack load bowtie
-bowtie-build ecoli.fq e_coli
+bowtie-build ecoli.fa e_coli
 EOF
